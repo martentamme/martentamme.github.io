f67774b15b22702405c1793af9a196f8cc23b45c
Marten Tamme <tammemarten@gmail.com>
2025-04-17 23:22:40 +0300

    add vue setup

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+   <v-app>
+    <v-main>
+      <v-container>
+          <p>{{"TEST"}}</p>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+</script>
+
+<style>
+</style>
